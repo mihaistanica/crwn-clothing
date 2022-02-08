5a1ece78dcffe7926fd55c726b2368308e8304a3
Mihai Stanica <mihai.stanica1@gmail.com>
2022-02-08 23:13:02 +0200

    created selectors and reducers for static data in shop collections and directory sections + added collection overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 
-import SHOP_DATA from "./shop.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-import './shop.styles.scss';
+import "./shop.styles.scss";
 
-import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super();
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>)
-    }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
