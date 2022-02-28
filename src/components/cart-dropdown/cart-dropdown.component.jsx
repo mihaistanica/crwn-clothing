b56a5a2820d3efdd388266dc326df78e1b96d0b3
Mihai Stanica <mihai.stanica1@gmail.com>
2022-02-28 21:41:59 +0200

    implemented STYLED for certain components and added logic that pulls in shop data from firestores and stores it into the shop reducer. also included WithSpinner HOC

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,16 +10,18 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import './cart-dropdown.styles.scss';
 
+import { CartDropdownContainer, CartItems, EmptyMessage } from "./cart-dropdown.styles";
+
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'>
+  <CartDropdownContainer>
+    <CartItems>
     {cartItems.length
       ? (cartItems.map(cartItem => (
       <CartItem key={cartItem.id} item={cartItem} />
     )))
-      : <span className='empty-message'>Your cart is empty</span>
+      : <EmptyMessage>Your cart is empty</EmptyMessage>
     }
-    </div>
+    </CartItems>
     <CustomButton
       onClick={
         () => {
@@ -29,7 +31,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
       }>
       GO TO CHECKOUT
     </CustomButton>
-  </div>
+  </CartDropdownContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,17 +8,51 @@ import { selectCollections } from "../../redux/shop/shop.selectors";
 import "./shop.styles.scss";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CategoryPage from "../collection/collection.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={ `${match.path}` } component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CategoryPage}/>
-  </div>
-);
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
+import { updateCollections } from '../../redux/shop/shop.actions.js';
 
-export default connect(mapStateToProps)(ShopPage);
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    collectionRef.get().then(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className="shop-page">
+        <Route exact path={ `${match.path}` } render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
+        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPage isLoading={loading} {...props} />}/>
+      </div>
+    );
+  }
+};
+
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage);
